5c545b581c253bdedf72f52f8af3db18737f5279
rawgroupchukera79-sacpe <rawgroupchukera79@gmail.com>
2026-09-24 12:23:58 +0500

    Update products.js

--- products.js
+++ products.js
@@ -1,1 +1,7 @@
-
+const products = [
+  { id: 1, name: "Shows - Bata", price: 3500, category: "Shoes", image: "logo.png" },
+  { id: 2, name: "Sout - Nishat", price: 5000, category: "Suits", image: "logo.png" },
+  { id: 3, name: "Bed sheet - R.A.W", price: 3500, category: "Bed Sheets", image: "logo.png" },
+  { id: 4, name: "Sout - ALI AND IJAZ", price: 5500, category: "Suits", image: "logo.png" },
+  { id: 5, name: "Shows - Bata", price: 4000, category: "Shoes", image: "logo.png" }
+];
